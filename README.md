2177cc24c134f41bbc64dbc06bfff15d22a8b509
MunSeongyun <borygashill608@gmail.com>
2025-07-12 20:53:04 +0900

    ✨ feat : auth, user 모듈 생성

---
+++ README.md
@@ -0,0 +1,3 @@
+# 실행방법
+1. .env.example 파일을 복사 후 이름을 .env로 변경
+1. docker compose up -d
--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
